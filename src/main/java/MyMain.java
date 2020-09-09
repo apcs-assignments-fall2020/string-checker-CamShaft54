14491c77747bd3b34f7747d3d9de4422ba387d7e
camshaft54 <cam.shaw1234@gmail.com>
2020-09-09 09:47:54 -0700

    Complete StringChecker

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,25 +5,40 @@ public class MyMain {
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
         // REPLACE WITH YOUR CODE
-        return -1;
+        return (str.length() - str.replace("a", "").length()) + (str.length() - str.replace("b", "").length()) + (str.length() - str.replace("c", "").length());
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
         // REPLACE WITH YOUR CODE
-        return false;
+        return str.toUpperCase().contains("THE");
     }
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        // REPLACE WITH YOUR CODE
+        return new StringBuilder(str).reverse().toString().equals(str);
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
+        System.out.println("Choose mode (countABC, containsThe, isPalindrome): ");
+        String menu = scan.next();
+        System.out.println("Enter String: ");
+        String str = scan.next();
+        switch (menu.toUpperCase()) {
+            case "COUNTABC":
+                System.out.println("countABC:\nInput: " + str + "\nOutput: " + countABC(str));
+                break;
+            case "CONTAINSTHE":
+                System.out.println("containsThe:\nInput: " + str + "\nOutput: " + containsThe(str));
+                break;
+            case "ISPALINDROME":
+                System.out.println("isPalindrome:\nInput: " + str + "\nOutput: " + isPalindrome(str));
+                break;
+            default:
+                System.out.println("Invalid Arguments.");
+        }
     }
 }
